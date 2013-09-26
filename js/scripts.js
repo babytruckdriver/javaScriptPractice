185285bbb65a6cdb06515cd7c32576112f0aa959
babytruckdriver <babytruckdriver@gmail.com>
2013-09-26 07:02:45 +0200

    Without comments

--- js/scripts.js
+++ js/scripts.js
@@ -155,7 +155,28 @@ var add_the_handlers = function (nodes) {
         }(i));
     }
 };
-add_the_handlers(document.body.childNodes);
+//add_the_handlers(document.body.childNodes);
+
+//Test 9 ----------------------------------------------
+String.method('deentityify', (function () {
+// The entity table. It maps entity names to
+// characters.
+    "use strict";
+    var entity = {
+        quot: '"',
+        lt:  '<',
+        gt: '>'
+    };
+// Return the deentityify method.
+    return function () {
+        return this.replace(/&([^&;]+);/g,
+            function (a, b) {
+                var r = entity[b];
+                return typeof r === 'string' ? r : a;
+            });
+    };
+}()));
+console.log('&lt;&quot;&gt;'.deentityify());
 
 
 //jQuery ----------------------------------------------
