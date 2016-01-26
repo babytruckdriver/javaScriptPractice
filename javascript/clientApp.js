bc88283a9601f7ca7f297f31c1fe7b8babfe07d7
babytruckdriver <babytruckdriver@gmail.com>
2016-01-26 09:21:19 +0100

    Adding comments to the inheritance examples

--- javascript/clientApp.js
+++ javascript/clientApp.js
@@ -1,7 +1,7 @@
 /*
  * Articles referenced:
  *   -https://medium.com/javascript-scene/common-misconceptions-about-inheritance-in-javascript-d5d9bab29b0a#.yepusunvp
- *   -
+ *   -https://medium.com/javascript-scene/master-the-javascript-interview-what-s-the-difference-between-class-prototypal-inheritance-e4cd0a7562e9#.2c18j7bff
  */
 
 define([], function () {
@@ -31,6 +31,7 @@ define([], function () {
       // ***********************************************************
 
       // Inheritance by concatenation
+      // We create a new object, {}, copying the prototype of another two, with Object.assign
       let mouse = Object.assign({}, animal, {
         furColor: "brown",
         legs: 4,
@@ -42,9 +43,9 @@ define([], function () {
       console.log(mouse.describe()); //mouse.animalType doesn't change
 
       // Inheritance by delegation
-      // The result object has its props and the props of the parent (no a copy of the props)
-      // 'fly' is just a new name for the extended animal object.
-      // For this kind of inheritance you need the "new" or "Object.create"
+      // The result object [fly] has its props and the props of the parents (no a copy of the props)
+      // 'fly' is just a new name for a extended animal object.
+      // For this kind of inheritance you need the "new" or "Object.create" keywords.
 
       let fly = Object.assign(Object.create(animal), {
         furColor: "black",
@@ -57,7 +58,7 @@ define([], function () {
       console.log(fly.describe()); // fly.animalType has changed
 
       // Functional inheritance
-      // You need create the a function factory and add new properties
+      // You need create a function factory and add new properties to the returned object
 
       let flyFactory = function () {
         return {
@@ -76,7 +77,7 @@ define([], function () {
     }
   };
 
-  //Se exporta la funcionalidad que se desea exponer
+  // We export the required functionality
   return {
     App: app
   };
